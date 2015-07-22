35eb5050e75da64bc9bd799c1b10d22a557e6071
Christopher Eyre <chriseyre2000@gmail.com>
2015-07-22 09:21:02 +0100

    Initial Demo Commit

---
+++ ConsoleDemo.Testing/DemoTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class DemoTest
+    {
+        [Test]
+        public void Basic()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
